9af03ab321aad589b6acd7ecbe771f0780fecafe
Abhishek <101618560+patilabhis-deloitte@users.noreply.github.com>
2023-06-05 13:31:12 +0530

    first commit

---
+++ src/main/java/com/example/SkillUp/SkillUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.SkillUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SkillUp/SkillUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SkillUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
